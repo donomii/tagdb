849d143567bc3dae0d6f1e078e200dce8181b32c
user <donomii@gmail.com>
2017-10-07 11:09:46 +0200

    Update windows build sscript

--- otherfiles/release.bat
+++ otherfiles/release.bat
@@ -1,6 +1,11 @@
 cd ..\release || exit 1
-go build ../cmd/tagshell/
-go build ../cmd/tagquery/
-go build ../cmd/tagserver/
-go build ../cmd/tagloader/
-go build ../cmd/fetchbot/
+mkdir tagdb
+copy README.md tagdb
+copy /r webfiles tagdb
+copy tagdb.conf tagdb
+cd tagdb
+go build ../../cmd/tagshell/
+go build ../../cmd/tagquery/
+go build ../../cmd/tagserver/
+go build ../../cmd/tagloader/
+go build ../../cmd/fetchbot/
