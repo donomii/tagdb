1356588308e0e2ff4b84d7832060ca5325e94d76
user <donomii@gmail.com>
2017-10-10 22:54:35 +0200

    Child process now shares parent stdin/stdout

--- tagbrowser/launcher_linux.go
+++ tagbrowser/launcher_linux.go
@@ -1,9 +1,13 @@
 package tagbrowser
 
+import "os"
 import "os/exec"
 import "fmt"
 
 func Launch(file string, line string) {
 	cmd := exec.Command("vim", file, fmt.Sprintf("+%v", line))
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
 	cmd.Run()
 }
--- tagbrowser/launcher_windows.go
+++ tagbrowser/launcher_windows.go
@@ -1,9 +1,13 @@
 package tagbrowser
 
+import "os"
 import "os/exec"
 
 func Launch(file string, line string) {
 	cmd := exec.Command("notepad/notepad++", file, "-n", line)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
 	//cmd := exec.Command("vim", file, fmt.Sprintf("+%v", line))
 	cmd.Run()
 }
