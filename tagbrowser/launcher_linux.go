a03e2f857291734613c59ba296eca4377b09ed7e
user <donomii@gmail.com>
2017-10-10 22:18:39 +0200

    Export correctly, fix signature

--- tagbrowser/launcher_linux.go
+++ tagbrowser/launcher_linux.go
@@ -3,7 +3,7 @@ package tagbrowser
 import "os/exec"
 import "fmt"
 
-func launch(file string, line int) {
+func Launch(file string, line string) {
 	cmd := exec.Command("vim", file, fmt.Sprintf("+%v", line))
 	cmd.Run()
 }
--- tagbrowser/launcher_windows.go
+++ tagbrowser/launcher_windows.go
@@ -2,7 +2,7 @@ package tagbrowser
 
 import "os/exec"
 
-func launch(file string, line int) {
+func Launch(file string, line string) {
 	cmd := exec.Command("notepad/notepad++", file, "-n", line)
 	//cmd := exec.Command("vim", file, fmt.Sprintf("+%v", line))
 	cmd.Run()
