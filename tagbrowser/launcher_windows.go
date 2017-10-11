b4d263e76a6e2bb00a0a53c7a3a93c97d6e9ec3a
user <donomii@gmail.com>
2017-10-11 23:14:51 +0200

    Launch notepad++ with the correct line option

--- tagbrowser/launcher_windows.go
+++ tagbrowser/launcher_windows.go
@@ -1,10 +1,11 @@
 package tagbrowser
 
 import "os"
+import "fmt"
 import "os/exec"
 
 func Launch(file string, line string) {
-	cmd := exec.Command("notepad/notepad++", file, "-n", line)
+	cmd := exec.Command("notepad/notepad++", file, fmt.Sprintf("-n%v", line))
 	//cmd := exec.Command("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", results[selection].Filename)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
